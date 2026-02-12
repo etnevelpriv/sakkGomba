0266be41d981af5f7cb9685226ed6defa68c5d0b
etnevelpriv <leventerastovits@gmail.com>
2026-02-12 07:24:24 +0100

    Add project files.

---
+++ sakkGomba/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace sakkGomba
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
