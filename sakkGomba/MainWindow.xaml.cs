daaa11faa14887f5e897f9c18effa19d12675640
etnevelpriv <leventerastovits@gmail.com>
2026-02-19 09:42:30 +0100

    Gridrol canvasra valtottam, babu poziciojat megnezu fuggveny kesz

--- sakkGomba/MainWindow.xaml.cs
+++ sakkGomba/MainWindow.xaml.cs
@@ -16,9 +16,60 @@ namespace sakkGomba
     /// </summary>
     public partial class MainWindow : Window
     {
+        string aktivBabu;
+        double babuXPozicio;
+        double babuYPozicio;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bastjaGomb_Click(object sender, RoutedEventArgs e)
+        {
+            aktivBabu = "bastja";
+            jatekInditasa();
+        }
+
+        private void futoGomb_Click(object sender, RoutedEventArgs e)
+        {
+            aktivBabu = "futo";
+            jatekInditasa();
+        }
+
+        private void huszarGomb_Click(object sender, RoutedEventArgs e)
+        {
+            aktivBabu = "huszar";
+            jatekInditasa();
+        }
+
+        private void kiralynoGomb_Click(object sender, RoutedEventArgs e)
+        {
+            aktivBabu = "kiralyno";
+            jatekInditasa();
+        }
+
+        private void jatekInditasa()
+        {
+            babuHatterBeallitasa();
+            jatekGrid.Visibility = Visibility.Visible;
+            menuGrid.Visibility = Visibility.Collapsed;
+        }
+
+        private void babuHatterBeallitasa()
+        {
+            var uri = new Uri($"pack://application:,,,/Images/{aktivBabu}.png", UriKind.Absolute);
+            babu.Fill = new ImageBrush(new BitmapImage(uri))
+            {
+                Stretch = Stretch.Uniform,
+                AlignmentX = AlignmentX.Center,
+                AlignmentY = AlignmentY.Center
+            };
+        }
+
+        private void checkBabuPozicio()
+        {
+            babuYPozicio = Canvas.GetTop(babu);
+            babuXPozicio = Canvas.GetLeft(babu);
         }
     }
 }
